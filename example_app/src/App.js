a7b0b01278140297dec6e8d9be264238461af8b5
Roy Mosby <ego.madking@gmail.com>
2022-12-18 22:02:35 -0500

    1.8 complete

--- example_app/src/App.js
+++ example_app/src/App.js
@@ -1,16 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import TodoList from './TodoList';
+import AddTodoForm from './AddTodoForm';
+
+const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Default`;
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
+      },
+    };
+    fetch(url, options)
+      .then((resp) => resp.json())
+      .then((data) => {
+        const todos = data.records.map((item) => ({
+          id: item.id,
+          title: item.fields.Title,
+        }));
+        setTodoList([...todos]);
+        setIsLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading) {
+      localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+    }
+  }, [todoList, isLoading]);
+
+  function addTodo(newTodo) {
+    setTodoList([...todoList, newTodo]);
+  }
+
+  function removeTodo(id) {
+    setTodoList(todoList.filter((todo) => todo.id !== id));
+  }
+
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+
+      <AddTodoForm onAddTodo={addTodo} />
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+      )}
     </div>
   );
 }

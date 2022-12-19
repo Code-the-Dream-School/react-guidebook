c3bf42dd08a864fcf8a286a126ce96eea7889e9d
Roy Mosby <ego.madking@gmail.com>
2022-12-18 21:51:06 -0500

    1.4 complete

--- example_app/src/App.js
+++ example_app/src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TodoList from './TodoList';
+import AddTodoForm from './AddTodoForm';
 
 function App() {
+
+function addTodo(newTodo){
+  setTodoList([...todoList, newTodo])
+}
+
+const [todoList, setTodoList] = useState([])
+
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <AddTodoForm onAddTodo={addTodo}/>
+      <TodoList todoList={todoList}/>
     </div>
   );
 }
